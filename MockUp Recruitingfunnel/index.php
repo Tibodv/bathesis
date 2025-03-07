fd8988a7bd9d3ac4ca09bc78e4ccc638bc0f688e
Tibo <tiboarenddevries@outlook.de>
2025-03-07 18:28:26 +0100

    disclaimer und randomizer hinzugefuegt

---
+++ MockUp Recruitingfunnel/index.php
@@ -0,0 +1,49 @@
+<?php
+// Randomly assign a value of 0 or 1
+$randomChoice = random_int(0, 1);
+
+
+$filename = 'group_counts.json';
+
+// Initialize the file if it doesn't exist
+if (!file_exists($filename)) {
+    file_put_contents($filename, json_encode(['groupA' => 0, 'groupB' => 0]));
+}
+
+
+// Open the file for reading and writing
+$file = fopen($filename, 'r+');
+if (flock($file, LOCK_EX)) { // acquire an exclusive lock
+    // Read the current data
+    $fileContents = file_get_contents($filename);
+    $data = json_decode($fileContents, true);
+    if (!is_array($data)) {
+        $data = ['groupA' => 0, 'groupB' => 0];
+    }
+    // Update the appropriate counter
+    if ($randomChoice === 0) {
+        $data['groupA']++;  //Group A: CB
+    } else {
+        $data['groupB']++; //Group B: No CB
+    }
+    // Write the updated data back to the file
+    ftruncate($file, 0);
+    rewind($file);
+    fwrite($file, json_encode($data));
+    fflush($file);
+    flock($file, LOCK_UN);
+}
+fclose($file);
+
+
+
+if ($randomChoice === 0){
+    header('Location: mockupCb.html');
+}
+elseif ($randomChoice === 1){
+    header('Location: mockupNoCb.html');
+
+}
+exit;
+
+?>
--- Notes/NotesBaThesis/05 Chatbot/Chatbot Q&A.md
+++ Notes/NotesBaThesis/05 Chatbot/Chatbot Q&A.md
@@ -1,16 +1,19 @@
 Welche Fragen könnten gestellt werden -> in die Source materials
 
 ---
-- Gehalt
-- Aufgaben
-- Arbeitsumfeld
-- Remote oder Lokal
-- Wo ist der Job ?
-- Was sind die Arbeitszeiten
-- Benefits
-- Career Development Opportunities
-- Interview (online/in-person)
-- tools and technology used
-- mission & vision
-- work life balance
-- 
+- [x] Gehalt
+- [x] Aufgaben
+- [ ] Arbeitsumfeld
+- [x] Remote oder Lokal
+- [x] Wo ist der Job ?
+- [x] Was sind die Arbeitszeiten
+- [ ] Benefits
+- [ ] Career Development Opportunities
+- [ ] Interview (online/in-person)
+- [ ] tools and technology used
+- [ ] mission & vision
+- [ ] work life balance
+- [ ] Teamgröße
+- [ ] Hierarchieebene
+
+ 
